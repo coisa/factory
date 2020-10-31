d4734595566bf7aea22e4e2ef2fbc07a08a39592
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2020-10-31 14:20:49 -0300

    Added ArgumentCountError

---
+++ src/Exception/ArgumentCountError.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of coisa/factory.
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ * @link      https://github.com/coisa/factory
+ * @copyright Copyright (c) 2020 Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
+namespace CoiSA\Factory\Exception;
+
+/**
+ * Class ArgumentCountError
+ *
+ * @package CoiSA\Factory\Exception
+ */
+final class ArgumentCountError extends \CoiSA\Exception\Core\ArgumentCountError implements FactoryExceptionInterface
+{
+}
