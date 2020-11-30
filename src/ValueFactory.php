785fbba374bb2a5edc9f99c37726baacfb53151d
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2020-11-30 04:23:32 -0300

    Added ValueFactory

---
+++ src/ValueFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of coisa/factory.
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ * @link      https://github.com/coisa/factory
+ *
+ * @copyright Copyright (c) 2020 Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+namespace CoiSA\Factory;
+
+/**
+ * Class ValueFactory
+ *
+ * @package CoiSA\Factory
+ */
+final class ValueFactory implements FactoryInterface
+{
+    /**
+     * @var mixed
+     */
+    private $value;
+
+    /**
+     * ValueFactory constructor.
+     *
+     * @param mixed $value
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function create()
+    {
+        return $this->value;
+    }
+}
